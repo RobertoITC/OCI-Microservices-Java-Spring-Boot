612a10e3e7093b3be7b042fc41a170455c12cdff
RobertoITC <a01642426@tec.mx>
2025-03-02 13:41:46 -0600

    Initial commit or update

---
+++ src/test/java/com/example/oci_microservice/App2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oci_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
